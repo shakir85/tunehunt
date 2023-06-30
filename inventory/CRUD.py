40dc8fae619506e219a9e63fe25332527d4d1024
shakir <sralmasaari@gmail.com>
2023-06-29 19:31:52 -0700

    Add crud functions

---
+++ inventory/CRUD.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+
+class InventoryCRUD:
+
+    def create_inventory(self, data: dict) -> None:
+        pass
+
+    def read_inventory(self, path: Path) -> dict:
+        pass
+
+    def update_inventory(self):
+        pass
+
+    def delete_inventory(self):
+        pass
